a976640667ee7b9e3fd59bdaf8a7746d54925b29
noah <gaoyong@xatu.edu.cn>
2020-02-15 23:31:38 +0800

    20200215-x

---
+++ blog/templatetags/__init__.py
@@ -0,0 +1,5 @@
+# _*_coding:utf-8_*_
+# Author : oracle12c
+# Time   : 2020/2/15 23:29
+# File   : __init__.py
+# IDE    : PyCharm
---
+++ blog/templatetags/blog_tags.py
@@ -0,0 +1,5 @@
+# _*_coding:utf-8_*_
+# Author : oracle12c
+# Time   : 2020/2/15 23:30
+# File   : blog_tags.py
+# IDE    : PyCharm
